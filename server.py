0915c93a1f9fc753e4a904f1220553b320574dcd
darkfelipe16-jpg <darkfelipe16@gmail.com>
2026-04-02 04:35:43 -0300

    server.py

--- server.py
+++ server.py
@@ -30,12 +30,13 @@ from googleapiclient.http import MediaIoBaseUpload, MediaIoBaseDownload
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / ".env")
+
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    cloudinary_url=os.getenv("CLOUDINARY_URL"),
     secure=True
 )
+
+print("CLOUDINARY_URL exists:", bool(os.getenv("CLOUDINARY_URL")))
 
 mongo_url = os.environ["MONGO_URL"]
 client = AsyncIOMotorClient(mongo_url)
